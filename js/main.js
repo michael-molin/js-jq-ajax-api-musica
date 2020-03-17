2ae093e6489336a72773d71a733f589e28869485
Michael <m.molin.1987@gmail.com>
2020-03-17 16:35:52 +0100

    Esercizio con bonus completato

---
+++ js/main.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+    var source = $("#card-template").html();
+    var cardTemplate = Handlebars.compile(source);
+
+    $('.select-genere').change(function() {
+        var genereSelezionato = $(this).val();
+        checkGenera(genereSelezionato);
+    });
+
+    $.ajax({
+        url: "https://flynn.boolean.careers/exercises/api/array/music",
+        method: "GET",
+        success: function(data){
+            var albums = data.response;
+            compilaMusica(albums);
+        },
+        error: function(data){
+            alert('ERRORE');
+        }
+    })
+
+    function compilaMusica(data) {
+        for (var i = 0; i < data.length; i++) {
+            var album = data[i];
+            var albumCompilato = {
+                immagineAlbum : album.poster,
+                nomeAlbum: album.title,
+                autore: album.author,
+                anno: album.year,
+                genere: album.genre
+            }
+            var cardCompilata = cardTemplate(albumCompilato);
+            $('.card-container').append(cardCompilata);
+        }
+    }
+
+    function checkGenera(genere) {
+        if (genere == "") {
+            $('.card').show();
+        } else {
+            $('.card').each(function() {
+                if (genere.toLowerCase() == $(this).data('genere').toLowerCase()) {
+                    $(this).show();
+                } else {
+                    $(this).hide();
+                }
+            });
+        }
+    }
+
+});
